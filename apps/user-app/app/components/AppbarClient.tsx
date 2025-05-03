7a893792d5c133d1ec9ac28d306b9056350f1700
Shofikul islam <shofikul.islam23102023@gmail.com>
2025-05-03 09:38:58 +0600

    fix0

--- apps/user-app/app/components/AppbarClient.tsx
+++ apps/user-app/app/components/AppbarClient.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Appbar } from "@repo/ui/Appbar";
 import { signIn, signOut, useSession } from "next-auth/react";
@@ -10,11 +10,15 @@ export function AppbarClient() {
   const router = useRouter();
 
   return (
-   <div>
-      <Appbar onSignin={signIn} onSignout={async () => {
-        await signOut()
-        router.push("/api/auth/signin")
-      }} user={session.data?.user} />
-   </div>
+    <div>
+      <Appbar
+        onSignin={signIn}
+        onSignout={async () => {
+          await signOut();
+          router.push("/api/auth/signin");
+        }}
+        user={session.data?.user}
+      />
+    </div>
   );
 }
